464a904d845c3f160e6fc3d6dea1b043b9dbe719
Bert van Brakel <bertvanbrakel@gmail.com>
2011-11-21 15:59:47 +0000

    Applied the Apache License 2.0

--- src/main/java/com/bertvanbrakel/lang/matcher/EqualsMatcher.java
+++ src/main/java/com/bertvanbrakel/lang/matcher/EqualsMatcher.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2011 Bert van Brakel
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.bertvanbrakel.lang.matcher;
 
 import org.apache.commons.lang3.builder.EqualsBuilder;
@@ -6,6 +21,7 @@ import org.apache.commons.lang3.builder.ToStringStyle;
 import org.hamcrest.Description;
 import org.hamcrest.TypeSafeMatcher;
 import static org.apache.commons.lang3.Validate.*;
+
 public class EqualsMatcher<T> extends TypeSafeMatcher<T>{
 
 	private final T expect;
