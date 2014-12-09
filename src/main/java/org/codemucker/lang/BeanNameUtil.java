8da371ba6f10106745b0c2ae91d7ea94a5302de5
Bert van Brakel <bertvanbrakel@gmail.com>
2014-12-09 13:32:49 +1030

    improv getter/setter name calculation, allow passing names starting with get/set/is

--- src/main/java/org/codemucker/lang/BeanNameUtil.java
+++ src/main/java/org/codemucker/lang/BeanNameUtil.java
@@ -2,21 +2,51 @@ package org.codemucker.lang;
 
 public class BeanNameUtil {
 
-    public static String toSetterName(String name) {
-        return "set" + ClassNameUtil.upperFirstChar(name);
-    }
+	public static String toSetterName(String name) {
+		name = stripPrefix(name);
+		return "set" + ClassNameUtil.upperFirstChar(name);
+	}
 
-    public static String toGetterName(String name, java.lang.reflect.Type type) {
-        boolean isBool = "boolean".equals(type.toString()) || "java.lang.Boolean".equals(type.toString());
-        return toGetterName(name, isBool);
-    }
+	public static String toGetterName(String name, java.lang.reflect.Type type) {
+		boolean isBool = "boolean".equals(type.toString())|| "java.lang.Boolean".equals(type.toString());
+		return toGetterName(name, isBool);
+	}
 
-    public static String toGetterName(String name, Class<?> type) {
-        return toGetterName(name, boolean.class.equals(type) || Boolean.class.equals(type));
-    }
+	public static String toGetterName(String name, Class<?> type) {
+		return toGetterName(name, boolean.class.equals(type) || Boolean.class.equals(type));
+	}
 
-    public static String toGetterName(String name, boolean isBoolean) {
-        return (isBoolean ? "is" : "get") + ClassNameUtil.upperFirstChar(name);
-    }
+	public static String toGetterName(String name, boolean isBoolean) {
+		name = stripPrefix(name);
+		return (isBoolean ? "is" : "get") + ClassNameUtil.upperFirstChar(name);
+	}
+
+	/**
+	 * Strips the get/set/is prefix from a method name 
+	 * 
+	 * <p>Handles the case where the prefix get/set/is is actually part of a name by ensuring that the letter after the prefix is in uppercase</p>
+	 * 
+	 * <p>Examples:
+	 * 	<ul>
+	 * 		<li>getFoo,setFoo,isFoo --%gt;  'Foo'
+	 * 		<li>gettyburg --&gt; gettyburg (unchanged)
+	 * 		<li>iso --&gt; iso (unchanged)
+	 * 		<li>getIso--&gt; iso
+	 * 		<li>isIso--&gt; iso
+	 * 		<li>isolated --&gt; isolated (unchanged)
+	 * 		<li>isIsolated --&gt; isolated
+	 * </p>
+	 * @param name
+	 * @return
+	 */
+	public static String stripPrefix(String name) {
+		if (name.startsWith("get") || name.startsWith("set") && (name.length() > 3  && Character.isUpperCase(name.charAt(4)))) {
+			return name.substring(3);
+		}
+		if (name.startsWith("is") && name.length() > 2 && Character.isUpperCase(name.charAt(3))) {
+			return name.substring(2);
+		}
+		return name;
+	}
 
 }
--- src/main/java/org/codemucker/lang/PathUtil.java
+++ src/main/java/org/codemucker/lang/PathUtil.java
@@ -6,21 +6,32 @@ import java.io.IOException;
 
 public class PathUtil {
 
-    public static String toForwardSlashes(String path) {
-        if (path == null) {
-            return null;
-        }
-        return path.replace('\\', '/');
-    }
-    
-    public static String filePathToClassPath(String path) {
-        if (path.charAt(0) == '/') {
-            return path.substring(1).replace('/', '.');
-        } else {
-            return path.replace('/', '.');
-        }
-    }
-    
+    /**
+     * Return the package name part of the given file path or null if no package name could be determined
+     */
+	public static String filePathToPackagePathOrNull(String path) {
+		path = toForwardSlashes(path);
+		if(path.charAt(0) == '/'){  //e.g   /Foo/Bar - > Foo/Bar, Foo/Bar --> Foo/Bar
+			path = path.substring(1);
+		}
+		int last = path.lastIndexOf('/'); 
+		if (last == -1 ) { //no package part
+			return null;
+		}
+		String pkg = path.substring(0, last); //e.g  Foo/Bar - > Foo
+		pkg = pkg.replace('/', '.');
+		if(pkg.length() == 0){
+			return null;
+		}
+		return pkg;
+	}
+
+	/**
+	 * Return the class name of the given java source or class file as suggested by the file path, or null if the class name could not be determined
+	 * 
+	 * @param filePathWithExtension
+	 * @return
+	 */
     public static String filePathToClassNameOrNull(String filePathWithExtension) {
         if (filePathWithExtension.endsWith(".java") || filePathWithExtension.endsWith(".class")) {
             String classPath = stripExtension(filePathWithExtension);
@@ -30,6 +41,22 @@ public class PathUtil {
         return null;
     }
 
+	private static String filePathToClassPath(String path) {
+		path = toForwardSlashes(path);
+		if(path.charAt(0) == '/'){
+			path = path.substring(1);
+		}
+		return path.replace('/', '.');
+	}
+	
+	public static String toForwardSlashes(String path) {
+        if (path == null) {
+            return null;
+        }
+        return path.replace('\\', '/');
+    }
+    
+    
     public static String stripExtension(String path) {            
     	int dot = path.lastIndexOf('.');
     	if (dot != -1) {
